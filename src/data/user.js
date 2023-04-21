a5efdb10bdde32db1986e2eb2bbeadd65aebaa1f
Sheila Seruya <Sheilaseruya@gmail.com>
2023-04-21 09:15:27 +0300

    done

--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,9 @@
-const username = "Liza";
-const city = "New York";
+export const username = "Liza";
 
-const image = "https://i.imgur.com/mV8PQxj.gif";
+export const city = "New York";
+
+export const image = "https://i.imgur.com/mV8PQxj.gif";
+
+
 
 // export all three variables as named exports
